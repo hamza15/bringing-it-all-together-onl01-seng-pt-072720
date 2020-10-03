fd077be59c7facc5c65e689246b360a3e43760ec
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-03 17:21:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,6 +27,14 @@ class Dog
   end
   
   def save
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs 
+      (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT)
+    SQL
+    DB[:conn].execute(sql)
     
   end
   
