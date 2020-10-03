fe2dfc7b16666033cb14b8ae3a0df1ff2fdb8209
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-03 17:32:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,7 +55,7 @@ class Dog
         SELECT * FROM dogs
         WHERE name = ?
       SQL
-      DB[:conn].execute(sql, name)[0][0]
+      DB[:conn].execute(sql, name)[0]
   end
   
   def self.find_or_create_by
