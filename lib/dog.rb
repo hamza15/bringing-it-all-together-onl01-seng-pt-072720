f4fa5f2d1aa2fcb98912eab88f952a2ee9f037bc
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-03 17:23:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,7 +28,7 @@ class Dog
   
   def save
     sql = <<-SQL
-      INSERT INTO songs (name, breed)
+      INSERT INTO dogs (name, breed)
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.breed)
