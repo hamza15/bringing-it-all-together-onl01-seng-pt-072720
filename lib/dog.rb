b5a42cc3c6f44925155c623cff8323a79f3e0823
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-03 17:27:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,10 +47,7 @@ class Dog
   end
   
   def self.new_from_db
-    sql = <<-SQL
-        SELECT * dogs (name, breed)
-        
-      SQL
+    Dog.new(id: row[0], name: row[1], breed: row[2])
   end
   
 end
